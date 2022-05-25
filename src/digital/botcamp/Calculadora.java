f1e1f484d6919f178a58e46b2be5129bb5d0d910
Gringss <kggrings@arezzo.com.br>
2022-05-25 13:16:21 -0300

    Aula DigitalInovation

---
+++ src/digital/botcamp/Calculadora.java
@@ -0,0 +1,25 @@
+package digital.botcamp;
+
+public class Calculadora {
+
+    public static void soma (double numero1, double numero2) {
+
+        double resultado = numero1 = numero2;
+
+        System.out.println("A soma entre " + numero1 + " mais " + numero2 + " é igual a " + resultado);
+    }
+
+    public static void subtracao (double numero1, double numero2){
+
+        double resultado = numero1 = numero2;
+
+        System.out.println("A soma entre " + numero1 + " mais " + numero2 + " é igual a " + resultado);
+    }
+
+    public static void multiplicacao (double numero1, double numero2){
+
+        double resultado = numero1 = numero2;
+
+        System.out.println("A soma entre " + numero1 + " mais " + numero2 + " é igual a " + resultado);
+    }
+}
---
+++ src/digital/botcamp/Main.java
@@ -0,0 +1,10 @@
+package digital.botcamp;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Cachorro c1 = new Cachorro("Thor", "White", (int) 2.0);
+
+        
+    }
+}
